e3f17da0f2701c7301867e7ea8f8d0bc54b5d175
nate <nate@nate.local>
2015-09-09 10:58:45 +0800

    Initial Commit

---
+++ Hello world/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello world
+//
+//  Created by nate on 15/9/9.
+//  Copyright (c) 2015年 nate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Hello world/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hello world
+//
+//  Created by nate on 15/9/9.
+//  Copyright (c) 2015年 nate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
